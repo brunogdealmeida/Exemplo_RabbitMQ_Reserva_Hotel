fbecc3d875dcf96194452f4739926061e69854ef
brunogdealmeida@gmail.com <brunogdealmeida@gmail.com>
2021-02-03 12:27:59 -0300

    Commit inicial do Exemplo

---
+++ AdministracaoHotel/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace AdministracaoHotel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost", UserName = "teste", Password = "teste" };
+
+            using (var connectionFactory = factory.CreateConnection()) 
+            using (var channel = connectionFactory.CreateModel())
+            {
+                var consumerMessage = new EventingBasicConsumer(channel);
+
+                consumerMessage.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+
+                    Console.WriteLine($"Hotel ABC Registry: {message}");
+                };
+
+                channel.BasicConsume(queue: "Registry", autoAck: true, consumer: consumerMessage);
+
+                Console.WriteLine("Para SAIR pressione Enter");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ ReservaHotel/Program.cs
@@ -0,0 +1,35 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace ReservaHotel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            Console.WriteLine("Olá, a reserva será feita para quantas pessoas ?");
+            count = Int32.Parse(Console.ReadLine());
+
+            var factory = new ConnectionFactory() { HostName = "localhost", UserName = "teste", Password = "teste" };
+
+            using (var connectionFactory = factory.CreateConnection())
+            using (var channel = connectionFactory.CreateModel())
+            {
+                while (count >= 1)
+                {
+                    var message = $"Reserva nº { Guid.NewGuid().ToString() } no Hotel ABC registrada com sucesso.";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "", routingKey: "Registry", basicProperties: null, body: body);
+
+                    Console.WriteLine($"Mensagem enviada com sucesso: {message}");
+                    Thread.Sleep(300);
+                    count--;
+                }
+            }
+        }
+    }
+}
